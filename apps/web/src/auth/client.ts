752e6b0d5b042746b60dd48dbf6dbbd06fc6eb4a
mavix21 <marcevizcarra2109@hotmail.com>
2026-01-21 14:34:31 -0500

    feat: add admin and organiztion client plugins to better auth client config

--- apps/web/src/auth/client.ts
+++ apps/web/src/auth/client.ts
@@ -1,11 +1,13 @@
 import { convexClient } from "@convex-dev/better-auth/client/plugins";
-import { siweClient } from "better-auth/client/plugins";
+import {
+  adminClient,
+  organizationClient,
+  siweClient,
+} from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
-import { siweWalletAgnosticClient } from "@buildea/better-auth-siwe-wallet-agnostic/client";
-
 export const authClient = createAuthClient({
-  plugins: [siweClient(), siweWalletAgnosticClient(), convexClient()],
+  plugins: [siweClient(), convexClient(), adminClient(), organizationClient()],
   fetchOptions: {
     credentials: "include",
   },
