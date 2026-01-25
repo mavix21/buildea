47541379b8a1b4bbbb6173bbf1d13fb36259c8b3
mavix21 <marcevizcarra2109@hotmail.com>
2026-01-25 00:15:22 -0500

    feat: update AppSidebar to include a dashboard link in the header

--- apps/web/app/_pages/dashboard/layout/ui/app-sidebar.tsx
+++ apps/web/app/_pages/dashboard/layout/ui/app-sidebar.tsx
@@ -13,6 +13,7 @@ import {
 } from "@buildea/ui/components/sidebar";
 
 import { preloadAuthQuery } from "@/auth/server";
+import { Link } from "@/shared/i18n";
 import { UserAvatarProfileSkeleton } from "@/widgets/auth/user-avatar-profile-skeleton";
 
 import SidebarMenuItems from "./sidebar-menu-items";
@@ -23,7 +24,16 @@ export default function AppSidebar() {
 
   return (
     <Sidebar variant="inset" collapsible="icon">
-      <SidebarHeader>{/* <OrgSwitcher /> */}</SidebarHeader>
+      <SidebarHeader className="@container">
+        <Suspense>
+          <Link
+            href="/dashboard"
+            className="dark:text-primary font-heading text-foreground cursor-pointer text-sm tracking-wide @min-[215px]:pl-2 @min-[215px]:text-4xl"
+          >
+            Buildea
+          </Link>
+        </Suspense>
+      </SidebarHeader>
       <SidebarContent className="overflow-x-hidden">
         <SidebarGroup>
           <SidebarGroupLabel>Overview</SidebarGroupLabel>
