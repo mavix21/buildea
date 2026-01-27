cfc09c220eb57917da0afcdd6f2a921ce75c26fb
mavix21 <marcevizcarra2109@hotmail.com>
2026-01-27 13:15:59 -0500

    feat: create index file for exporting profile-related components

---
+++ apps/web/app/_pages/dashboard/builder-profile/ui/index.ts
@@ -0,0 +1,8 @@
+export { ProfileContent } from "./profile-content";
+export { ProfileHeader } from "./profile-header";
+export { ProfileNotFound } from "./profile-not-found";
+export { ProfileSkeleton } from "./profile-skeleton";
+export { ProfileStats } from "./profile-stats";
+export { ProfileTabs } from "./profile-tabs";
+export { AchievementsCard } from "./achievements-card";
+export { SkillsCard } from "./skills-card";
